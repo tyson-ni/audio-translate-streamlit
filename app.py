8827350ab057a77868f3c821644b74e1228a5f8a
Tyson Ni <tysonzyni@gmail.com>
2025-04-18 14:54:27 -0700

    update API calls to latest syntax

--- app.py
+++ app.py
@@ -1,39 +1,44 @@
 import streamlit as st
-import openai
 import os
+from openai import OpenAI
+
+client = OpenAI()
 
 if 'OPENAI_API_KEY' in os.environ:
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    client.api_key = os.getenv("OPENAI_API_KEY")
 else:
-    openai.api_key = st.secrets['OPENAI_API_KEY']
+    client.api_key = st.secrets['OPENAI_API_KEY']
 
 # Function to identify text using GPT-4
 def identify_and_translate(text):
-    completion = openai.OpenAI().chat.completions.create(
-        model = 'gpt-4',
-        messages = [
+    response = client.responses.create(
+        model = 'gpt-4.1',
+        input = [
             {'role': 'system', 'content': """
-                You will identify the language of the text and translate it into English. 
+                You will identify the language of the text and translate it into English. If the conversation 
                 <Example>
                 User Input: 我明天打算在家休息。
-                Language: Chinese
-                Translation: I plan to rest at home tomorrow.
+                Language: Chinese /n
+                Translation: I plan to rest at home tomorrow. /n
+                Transcription: <Original Text>
                 </Example>
              """},
             {'role': 'user', 'content': "Text: {}".format(text)}
         ],
         temperature=0.0
     )
-    return  completion.choices[0].message.content
+    return  response.output_text
 
 # Function to translate and transcribe audio using Whisper API
 def process_audio(audio):
     # Transcribe audio
-    transcription = openai.OpenAI().audio.transcriptions.create(file=audio, model="whisper-1", response_format="text")
+    transcription = client.audio.transcriptions.create(
+        model="gpt-4o-transcribe",
+        file=audio
+    )
     # Identify language and translate
-    translation = identify_and_translate(transcription)
-    output = '{}'.format(translation)
-    return(output)
+    translation = identify_and_translate(transcription.text)
+    return(translation)
 
 # Main section
 st.header("Audio Translate and Transcribe")
